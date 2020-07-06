93a8cc88eb18b0964d86ba85de8b20564af07462
Radiation-Transport <48320561+Radiation-Transport@users.noreply.github.com>
2020-07-06 14:33:57 +0200

    Add files via upload
    Update headers.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@
 # OR CONDITIONS OF ANY KIND, either express or implied. See the Licence permissions                    #
 # and limitations under the Licence.                                                                   #
 ########################################################################################################
-@author: Alvaro
+@author: Alvaro Cubi
 """
 from functions import trajectory, vtk_file
 
